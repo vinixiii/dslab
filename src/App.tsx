5c97b64d13b283fdbb1f5d606c44431d171c2940
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-10-11 21:38:20 -0300

    feat: change hello world style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/global.css';
 
 export function App() {
-  return <h1 className="font-bold text-5xl text-violet-500">Hello world!</h1>;
+  return <h1 className="font-bold text-2xl text-black">Hello world!</h1>;
 }
