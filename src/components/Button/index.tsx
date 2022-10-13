77ba4c07bc7a5f62e24e689260cba2e875ea8569
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-10-12 23:06:34 -0300

    feat: add class name prop to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,27 +1,34 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
-interface ButtonPropsWithAsChild {
+interface CommonButtonProps {
+  className?: string;
+}
+
+interface ButtonPropsWithAsChild extends CommonButtonProps {
   asChild: true;
   children: ReactNode;
 }
 
-interface ButtonPropsWithoutAsChild {
+interface ButtonPropsWithoutAsChild extends CommonButtonProps {
   asChild?: never;
   children: string;
 }
 
-export type ButtonProps = ButtonPropsWithAsChild | ButtonPropsWithoutAsChild;
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  (ButtonPropsWithAsChild | ButtonPropsWithoutAsChild);
 
-export function Button({ asChild, children }: ButtonProps) {
+export function Button({ className, asChild, children, ...rest }: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
       className={clsx(
-        'py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 ring-white outline-none',
+        'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full hover:bg-cyan-300 transition-colors focus:ring-2 ring-white outline-none',
+        className,
       )}
+      {...rest}
     >
       {children}
     </Comp>
