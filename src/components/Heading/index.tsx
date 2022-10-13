b56297b889bef4d8da0868d021562d4dbc623551
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2022-10-12 22:50:40 -0300

    feat: pass class name prop to heading and text components

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,6 +4,7 @@ import { Slot } from '@radix-ui/react-slot';
 
 interface CommonHeadingProps {
   size?: 'sm' | 'md' | 'lg';
+  className?: string;
 }
 
 interface HeadingPropsWithAsChild extends CommonHeadingProps {
@@ -18,16 +19,25 @@ interface HeadingPropsWithoutAsChild extends CommonHeadingProps {
 
 export type HeadingProps = HeadingPropsWithAsChild | HeadingPropsWithoutAsChild;
 
-export function Heading({ size = 'md', asChild, children }: HeadingProps) {
+export function Heading({
+  size = 'md',
+  className,
+  asChild,
+  children,
+}: HeadingProps) {
   const Comp = asChild ? Slot : 'h2';
 
   return (
     <Comp
-      className={clsx('text-gray-100 font-sans font-bold', {
-        'text-lg': size === 'sm',
-        'text-xl': size === 'md',
-        'text-2xl': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-sans font-bold',
+        {
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
+        },
+        className,
+      )}
     >
       {children}
     </Comp>
--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -4,6 +4,7 @@ import { Slot } from '@radix-ui/react-slot';
 
 interface CommonTextProps {
   size?: 'sm' | 'md' | 'lg';
+  className?: string;
 }
 
 interface TextPropsWithAsChild extends CommonTextProps {
@@ -18,16 +19,20 @@ interface TextPropsWithoutAsChild extends CommonTextProps {
 
 export type TextProps = TextPropsWithAsChild | TextPropsWithoutAsChild;
 
-export function Text({ size = 'md', asChild, children }: TextProps) {
+export function Text({ size = 'md', className, asChild, children }: TextProps) {
   const Comp = asChild ? Slot : 'span';
 
   return (
     <Comp
-      className={clsx('text-gray-100 font-sans', {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg',
-      })}
+      className={clsx(
+        'text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        },
+        className,
+      )}
     >
       {children}
     </Comp>
